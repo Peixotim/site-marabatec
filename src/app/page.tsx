467d624bcecb6448b37ee8e7d493fb241fbbdcd8
Peixotim <pedropeixotovz@gmail.com>
2025-10-09 13:48:44 -0300

    feat: Troca cursos por areas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
             <section id="cursos" className="py-12 lg:py-12">
               <div className="max-w-7xl mx-auto px-4">
                 <h2 className="text-3xl font-bold text-black mb-8 text-center">
-                  Cursos em Destaque
+                  Áreas em Destaque
                 </h2>
                 <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
                   {filteredCards.map((card, i) => (
